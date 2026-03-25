304c0ab2aea5bf8e4913480741b19ed7cf807d6e
artembredak <artembredakk@gmail.com>
2026-03-26 00:29:15 +0100

    initial commit

---
+++ src/main/java/com/artembredak/sporteventscalendar/SportEventsCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.artembredak.sporteventscalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEventsCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportEventsCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artembredak/sporteventscalendar/SportEventsCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artembredak.sporteventscalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportEventsCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
